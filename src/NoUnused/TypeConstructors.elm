9772a67bc098f2e51c830db8289f096346e56e78
Jeroen Engels <jfm.engels@gmail.com>
2019-08-28 09:30:13 +0200

    Copy NoUnused.TypeConstructors from the elm-lint project

---
+++ src/NoUnused/TypeConstructors.elm
@@ -0,0 +1,202 @@
+module NoUnused.TypeConstructors exposing (rule)
+
+{-| Forbid having unused custom type constructors in a file.
+
+
+# Rule
+
+@docs rule
+
+-}
+
+import Dict exposing (Dict)
+import Elm.Syntax.Declaration exposing (Declaration(..))
+import Elm.Syntax.Exposing exposing (Exposing(..), TopLevelExpose(..))
+import Elm.Syntax.Expression exposing (Expression(..))
+import Elm.Syntax.Module as Module exposing (Module(..))
+import Elm.Syntax.Node as Node exposing (Node)
+import Elm.Syntax.TypeAnnotation exposing (TypeAnnotation(..))
+import Lint.Rule as Rule exposing (Direction, Error, Rule)
+import Set exposing (Set)
+
+
+{-| Forbid having unused custom type constructors in a file.
+
+    config =
+        [ NoUnused.TypeConstructors.rule
+        ]
+
+Note that this does not report a constructor if it is exposed in the module, even
+if it is not used anywhere in the project. For a more accurate detection of
+unused constructors (and functions) accross your project, you might want to check
+out [elm-xref](https://github.com/zwilias/elm-xref). You may still want to use
+this rule in your config so that you get notified of unused constructors earlier
+in your editor, rather than when running your tests or [elm-xref](https://github.com/zwilias/elm-xref).
+
+
+## Fail
+
+    module A exposing (a)
+
+    type MyType
+        = UsedType
+        | UnusedType -- Will get reported
+
+    a =
+        UsedType
+
+
+## Success
+
+    module A exposing (ExposedType(..))
+
+    type MyType
+        = UsedType
+
+    a =
+        UsedType
+
+    type ExposedType
+        = A
+        | B
+        | C
+
+    -----------------------
+    module A exposing (..)
+
+    type ExposedType
+        = A
+        | B
+        | C
+
+-}
+rule : Rule
+rule =
+    Rule.newSchema "NoUnused.TypeConstructors"
+        |> Rule.withInitialContext initialContext
+        |> Rule.withModuleDefinitionVisitor moduleDefinitionVisitor
+        |> Rule.withDeclarationVisitor declarationVisitor
+        |> Rule.withExpressionVisitor expressionVisitor
+        |> Rule.withFinalEvaluation finalEvaluation
+        |> Rule.fromSchema
+
+
+type alias Context =
+    { exposedCustomTypesWithConstructors : Set String
+    , exposesEverything : Bool
+    , declaredTypesWithConstructors : Dict String (Node String)
+    , usedFunctionOrValues : Set String
+    }
+
+
+initialContext : Context
+initialContext =
+    { exposedCustomTypesWithConstructors = Set.empty
+    , exposesEverything = False
+    , declaredTypesWithConstructors = Dict.empty
+    , usedFunctionOrValues = Set.empty
+    }
+
+
+error : Node String -> Error
+error node =
+    Rule.error
+        { message = "Type constructor `" ++ Node.value node ++ "` is not used."
+        , details = [ "Since it is not being used, I recommend removing it. It should make the code clearer to read for other people." ]
+        }
+        (Node.range node)
+
+
+moduleDefinitionVisitor : Node Module -> Context -> ( List Error, Context )
+moduleDefinitionVisitor moduleNode context =
+    case Module.exposingList (Node.value moduleNode) of
+        All _ ->
+            ( [], { context | exposesEverything = True } )
+
+        Explicit list ->
+            let
+                names : List String
+                names =
+                    List.filterMap
+                        (\node ->
+                            case Node.value node of
+                                TypeExpose { name, open } ->
+                                    case open of
+                                        Just _ ->
+                                            Just name
+
+                                        Nothing ->
+                                            Nothing
+
+                                _ ->
+                                    Nothing
+                        )
+                        list
+            in
+            ( []
+            , { context
+                | exposedCustomTypesWithConstructors =
+                    Set.union (Set.fromList names) context.exposedCustomTypesWithConstructors
+              }
+            )
+
+
+declarationVisitor : Node Declaration -> Direction -> Context -> ( List Error, Context )
+declarationVisitor node direction context =
+    case ( direction, Node.value node ) of
+        ( Rule.OnEnter, CustomTypeDeclaration { name, constructors } ) ->
+            if Set.member (Node.value name) context.exposedCustomTypesWithConstructors then
+                ( [], context )
+
+            else
+                let
+                    newContext : Context
+                    newContext =
+                        List.foldl
+                            (\constructor ctx ->
+                                let
+                                    nameNode : Node String
+                                    nameNode =
+                                        (Node.value constructor).name
+                                in
+                                { ctx
+                                    | declaredTypesWithConstructors =
+                                        Dict.insert
+                                            (Node.value nameNode)
+                                            nameNode
+                                            ctx.declaredTypesWithConstructors
+                                }
+                            )
+                            context
+                            constructors
+                in
+                ( [], newContext )
+
+        _ ->
+            ( [], context )
+
+
+expressionVisitor : Node Expression -> Direction -> Context -> ( List Error, Context )
+expressionVisitor node direction context =
+    if context.exposesEverything then
+        ( [], context )
+
+    else
+        case ( direction, Node.value node ) of
+            ( Rule.OnEnter, FunctionOrValue [] name ) ->
+                ( [], { context | usedFunctionOrValues = Set.insert name context.usedFunctionOrValues } )
+
+            _ ->
+                ( [], context )
+
+
+finalEvaluation : Context -> List Error
+finalEvaluation context =
+    if context.exposesEverything then
+        []
+
+    else
+        context.declaredTypesWithConstructors
+            |> Dict.filter (\name _ -> not <| Set.member name context.usedFunctionOrValues)
+            |> Dict.toList
+            |> List.map (\( _, node ) -> error node)
---
+++ tests/NoUnusedTypeConstructorsTest.elm
@@ -0,0 +1,82 @@
+module NoUnusedTypeConstructorsTest exposing (all)
+
+import Lint.Test exposing (LintResult)
+import NoUnused.TypeConstructors exposing (rule)
+import Test exposing (Test, describe, test)
+
+
+testRule : String -> LintResult
+testRule =
+    Lint.Test.run rule
+
+
+details : List String
+details =
+    [ "Since it is not being used, I recommend removing it. It should make the code clearer to read for other people."
+    ]
+
+
+tests : List Test
+tests =
+    [ test "should not report non-exposed variables" <|
+        \() ->
+            testRule """module A exposing (b)
+a = 1"""
+                |> Lint.Test.expectNoErrors
+    , test "should not report used type constructors" <|
+        \() ->
+            testRule """module A exposing (b)
+type Foo = Bar | Baz
+a = Bar
+b = Baz"""
+                |> Lint.Test.expectNoErrors
+    , test "should not report unused type constructors when module is exposing all" <|
+        \() ->
+            testRule """module A exposing (..)
+type Foo = Bar | Baz
+"""
+                |> Lint.Test.expectNoErrors
+    , test "should not report unused type constructors when module is exposing the constructors of that type" <|
+        \() ->
+            testRule """module A exposing (Foo(..))
+type Foo = Bar | Baz
+"""
+                |> Lint.Test.expectNoErrors
+    , test "should report unused type constructors" <|
+        \() ->
+            testRule """module A exposing (b)
+type Foo = Bar | Baz"""
+                |> Lint.Test.expectErrors
+                    [ Lint.Test.error
+                        { message = "Type constructor `Bar` is not used."
+                        , details = details
+                        , under = "Bar"
+                        }
+                    , Lint.Test.error
+                        { message = "Type constructor `Baz` is not used."
+                        , details = details
+                        , under = "Baz"
+                        }
+                    ]
+    , test "should report unused type constructors, even if the type is exposed" <|
+        \() ->
+            testRule """module A exposing (Foo)
+type Foo = Bar | Baz"""
+                |> Lint.Test.expectErrors
+                    [ Lint.Test.error
+                        { message = "Type constructor `Bar` is not used."
+                        , details = details
+                        , under = "Bar"
+                        }
+                    , Lint.Test.error
+                        { message = "Type constructor `Baz` is not used."
+                        , details = details
+                        , under = "Baz"
+                        }
+                    ]
+    ]
+
+
+all : Test
+all =
+    describe "NoUnusedTypeConstructors" tests
