eb67dc4acdb407ea7280d7dfe40b6e82c4a42d18
Jeroen Engels <jfm.engels@gmail.com>
2020-09-05 20:38:12 +0200

    Remove new rule from example/

--- example/src/ReviewConfig.elm
+++ example/src/ReviewConfig.elm
@@ -11,7 +11,6 @@ when inside the directory containing this file.
 
 -}
 
-import NoUnused.CustomTypeConstructorArgs
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
 import NoUnused.Exports
@@ -25,7 +24,6 @@ import Review.Rule exposing (Rule)
 config : List Rule
 config =
     [ NoUnused.CustomTypeConstructors.rule []
-    , NoUnused.CustomTypeConstructorArgs.rule
     , NoUnused.Dependencies.rule
     , NoUnused.Exports.rule
     , NoUnused.Modules.rule
