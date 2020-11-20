5e86b97bfcadd944b47b4bbf3b7bde8408f7371d
Jeroen Engels <jfm.engels@gmail.com>
2020-11-20 23:01:06 +0100

    Add tests to report unused fields for arguments

--- tests/NoUnused/RecordFieldsTest.elm
+++ tests/NoUnused/RecordFieldsTest.elm
@@ -75,4 +75,33 @@ b = a.foo
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectNoErrors
+        , test "should report unused fields of argument (using record access)" <|
+            \() ->
+                """module A exposing (a)
+a : {foo:Int, unused:Int} -> Int
+a arg = arg.foo
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Unused field `unused`"
+                            , details = [ "REPLACEME" ]
+                            , under = "unused"
+                            }
+                        ]
+        , Test.skip <|
+            test "should report unused fields of argument (using destructuring)" <|
+                \() ->
+                    """module A exposing (a)
+a : {foo:Int, unused:Int} -> Int
+a arg = arg.foo
+"""
+                        |> Review.Test.run rule
+                        |> Review.Test.expectErrors
+                            [ Review.Test.error
+                                { message = "Unused field `unused`"
+                                , details = [ "REPLACEME" ]
+                                , under = "unused"
+                                }
+                            ]
         ]
