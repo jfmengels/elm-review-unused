1b7b68c37653b2a8102cd70957febe2e5ad784c2
Jeroen Engels <jfm.engels@gmail.com>
2020-11-22 21:46:35 +0100

    Revert "Mark as used"
    This reverts commit 276560e28671cd9646fcfccb559f01a51a5501fb.

--- src/NoUnused/RecordFields.elm
+++ src/NoUnused/RecordFields.elm
@@ -530,7 +530,7 @@ expressionEnterVisitor node context =
                 ( [], updateRegister name Variable.markAsUsedInAnUnknownManner context )
 
         Expression.RecordUpdateExpression name _ ->
-            ( [], updateRegister (Node.value name) Variable.markAsUsed context )
+            ( [], updateRegister (Node.value name) Variable.markAsUsedInAnUnknownManner context )
 
         Expression.LetExpression letBlock ->
             let
