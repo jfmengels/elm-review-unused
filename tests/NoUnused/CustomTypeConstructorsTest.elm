ce9296f2372ba8536875a7decf45a8998fd15e30
Jeroen Engels <jfm.engels@gmail.com>
2021-04-06 22:55:31 +0200

    only

--- tests/NoUnused/CustomTypeConstructorsTest.elm
+++ tests/NoUnused/CustomTypeConstructorsTest.elm
@@ -126,25 +126,24 @@ a = Bar
 b = Baz"""
                     |> Review.Test.runWithProjectData project (rule [])
                     |> Review.Test.expectNoErrors
-        , Test.only <|
-            test "should report unused type constructors" <|
-                \() ->
-                    """
+        , test "should report unused type constructors" <|
+            \() ->
+                """
 module MyModule exposing (b)
 type Foo = Bar | Baz"""
-                        |> Review.Test.runWithProjectData project (rule [])
-                        |> Review.Test.expectErrors
-                            [ Review.Test.error
-                                { message = "Type constructor `Bar` is not used."
-                                , details = [ defaultDetails ]
-                                , under = "Bar"
-                                }
-                            , Review.Test.error
-                                { message = "Type constructor `Baz` is not used."
-                                , details = [ defaultDetails ]
-                                , under = "Baz"
-                                }
-                            ]
+                    |> Review.Test.runWithProjectData project (rule [])
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Type constructor `Bar` is not used."
+                            , details = [ defaultDetails ]
+                            , under = "Bar"
+                            }
+                        , Review.Test.error
+                            { message = "Type constructor `Baz` is not used."
+                            , details = [ defaultDetails ]
+                            , under = "Baz"
+                            }
+                        ]
         , test "should report unused type constructors, even if the type is exposed" <|
             \() ->
                 """
@@ -694,25 +693,26 @@ type Foo = Bar | Baz
                             , under = "Baz"
                             }
                         ]
-        , test "should report unused type constructors when application module is exposing the constructors" <|
-            \() ->
-                """
+        , Test.only <|
+            test "should report unused type constructors when application module is exposing the constructors" <|
+                \() ->
+                    """
 module MyModule exposing (Foo(..))
 type Foo = Bar | Baz
 """
-                    |> Review.Test.runWithProjectData applicationProject (rule [])
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "Type constructor `Bar` is not used."
-                            , details = [ defaultDetails ]
-                            , under = "Bar"
-                            }
-                        , Review.Test.error
-                            { message = "Type constructor `Baz` is not used."
-                            , details = [ defaultDetails ]
-                            , under = "Baz"
-                            }
-                        ]
+                        |> Review.Test.runWithProjectData applicationProject (rule [])
+                        |> Review.Test.expectErrors
+                            [ Review.Test.error
+                                { message = "Type constructor `Bar` is not used."
+                                , details = [ defaultDetails ]
+                                , under = "Bar"
+                                }
+                            , Review.Test.error
+                                { message = "Type constructor `Baz` is not used."
+                                , details = [ defaultDetails ]
+                                , under = "Baz"
+                                }
+                            ]
         ]
 
 
