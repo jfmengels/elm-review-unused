fa180c10fac3aefdb29530f86bc0fcd4f0422fa6
Jeroen Engels <jfm.engels@gmail.com>
2021-04-06 22:33:43 +0200

    only

--- tests/NoUnused/CustomTypeConstructorsTest.elm
+++ tests/NoUnused/CustomTypeConstructorsTest.elm
@@ -693,25 +693,26 @@ type Foo = Bar | Baz
                             , under = "Baz"
                             }
                         ]
-        , test "should report unused type constructors when application module is exposing the constructors" <|
-            \() ->
-                """
+        , Test.only <|
+            test "should report unused type constructors when application module is exposing the constructors" <|
+                \() ->
+                    """
 module MyModule exposing (Foo(..))
 type Foo = Bar | Baz
 """
-                    |> Review.Test.runWithProjectData applicationProject (rule [])
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "Type constructor `Bar` is not used."
-                            , details = [ defaultDetails ]
-                            , under = "Bar"
-                            }
-                        , Review.Test.error
-                            { message = "Type constructor `Baz` is not used."
-                            , details = [ defaultDetails ]
-                            , under = "Baz"
-                            }
-                        ]
+                        |> Review.Test.runWithProjectData applicationProject (rule [])
+                        |> Review.Test.expectErrors
+                            [ Review.Test.error
+                                { message = "Type constructor `Bar` is not used."
+                                , details = [ defaultDetails ]
+                                , under = "Bar"
+                                }
+                            , Review.Test.error
+                                { message = "Type constructor `Baz` is not used."
+                                , details = [ defaultDetails ]
+                                , under = "Baz"
+                                }
+                            ]
         ]
 
 
