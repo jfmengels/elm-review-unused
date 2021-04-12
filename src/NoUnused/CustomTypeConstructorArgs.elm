68a58760926d6b1d5a70be5c4615a3480ecc611a
Jeroen Engels <jfm.engels@gmail.com>
2021-04-12 10:02:56 +0200

    Fix typo in docs

--- src/NoUnused/CustomTypeConstructorArgs.elm
+++ src/NoUnused/CustomTypeConstructorArgs.elm
@@ -60,7 +60,7 @@ This rule attempts to detect when the custom type is used in comparisons, but it
 
 ## When not to enable this rule?
 
-If you like giving names to all arguments when pattern matching, then this rule will not found many problems.
+If you like giving names to all arguments when pattern matching, then this rule will not find many problems.
 This rule will work well when enabled along with [`NoUnused.Patterns`](./NoUnused-Patterns).
 
 Also, if you like comparing custom types in the way described above, you might pass on this rule, or want to be very careful when enabling it.
