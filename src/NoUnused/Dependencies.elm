6eaef55b91d756fabfd2665caf4e10f60e8be750
Jeroen Engels <jfm.engels@gmail.com>
2021-04-13 22:56:45 +0200

    Turn into records

--- src/NoUnused/Dependencies.elm
+++ src/NoUnused/Dependencies.elm
@@ -314,8 +314,8 @@ error elmJsonKey dependencies packageNameStr =
 
 
 type ProjectAndDependencyIdentifier
-    = ApplicationProject ( Elm.Project.ApplicationInfo, Elm.Package.Name, Elm.Version.Version )
-    | PackageProject ( Elm.Project.PackageInfo, Elm.Package.Name, Elm.Constraint.Constraint )
+    = ApplicationProject { project : Elm.Project.ApplicationInfo, name : Elm.Package.Name, version : Elm.Version.Version }
+    | PackageProject { project : Elm.Project.PackageInfo, name : Elm.Package.Name, constraint : Elm.Constraint.Constraint }
 
 
 fromProject : Project -> String -> Maybe ProjectAndDependencyIdentifier
@@ -325,7 +325,7 @@ fromProject project packageNameStr =
             -- TODO Make it possible to look at test dependencies
             case find (isPackageWithName packageNameStr) application.depsDirect of
                 Just ( packageName, version ) ->
-                    Just (ApplicationProject ( application, packageName, version ))
+                    Just (ApplicationProject { project = application, name = packageName, version = version })
 
                 Nothing ->
                     Nothing
@@ -334,7 +334,7 @@ fromProject project packageNameStr =
             -- TODO Make it possible to look at test dependencies
             case find (isPackageWithName packageNameStr) packageInfo.deps of
                 Just ( packageName, constraint ) ->
-                    Just (PackageProject ( packageInfo, packageName, constraint ))
+                    Just (PackageProject { project = packageInfo, name = packageName, constraint = constraint })
 
                 Nothing ->
                     Nothing
@@ -343,11 +343,11 @@ fromProject project packageNameStr =
 toProject : ProjectAndDependencyIdentifier -> Elm.Project.Project
 toProject projectAndDependencyIdentifier =
     case projectAndDependencyIdentifier of
-        ApplicationProject ( applicationInfo, _, _ ) ->
-            Elm.Project.Application applicationInfo
+        ApplicationProject { project } ->
+            Elm.Project.Application project
 
-        PackageProject ( packageInfo, _, _ ) ->
-            Elm.Project.Package packageInfo
+        PackageProject { project } ->
+            Elm.Project.Package project
 
 
 removeProjectDependency : Dict String Dependency -> String -> Project -> Project
