a64578bcf5a4b2c6e635b367bf12f60b71530b91
Jeroen Engels <jfm.engels@gmail.com>
2021-04-13 21:37:44 +0200

    Format documentation

--- src/NoUnused/Dependencies.elm
+++ src/NoUnused/Dependencies.elm
@@ -363,7 +363,9 @@ isADependencyOfAnotherDependency packageName deps dependencies =
 
 {-| Find the first element that satisfies a predicate and return
 Just that element. If none match, return Nothing.
-find (\\num -> num > 5) [2, 4, 6, 8] == Just 6
+
+    find (\num -> num > 5) [ 2, 4, 6, 8 ] == Just 6
+
 -}
 find : (a -> Bool) -> List a -> Maybe a
 find predicate list =
