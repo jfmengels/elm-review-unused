6e086c4777da58bf9506c55c522e374bc8e7f1ca
Jeroen Engels <jfm.engels@gmail.com>
2021-07-26 17:53:57 +0200

    Move declarations

--- tests/NoUnused/ModulesTest.elm
+++ tests/NoUnused/ModulesTest.elm
@@ -15,135 +15,6 @@ all =
     describe "NoUnusedModules" tests
 
 
-application : Project
-application =
-    Project.new
-        |> Project.addElmJson applicationElmJson
-
-
-lamderaApplication : Project
-lamderaApplication =
-    Project.new
-        |> Project.addElmJson lamderaApplicationElmJson
-
-
-applicationElmJson : { path : String, raw : String, project : Elm.Project.Project }
-applicationElmJson =
-    { path = "elm.json"
-    , raw = """{
-    "type": "application",
-    "source-directories": [
-        "src"
-    ],
-    "elm-version": "0.19.1",
-    "dependencies": {
-        "direct": {
-            "elm/core": "1.0.0"
-        },
-        "indirect": {}
-    },
-    "test-dependencies": {
-        "direct": {},
-        "indirect": {}
-    }
-}"""
-    , project =
-        Elm.Project.Application
-            { elm = Elm.Version.one
-            , dirs = []
-            , depsDirect = [ ( unsafePackageName "elm/core", Elm.Version.one ) ]
-            , depsIndirect = []
-            , testDepsDirect = []
-            , testDepsIndirect = []
-            }
-    }
-
-
-lamderaApplicationElmJson : { path : String, raw : String, project : Elm.Project.Project }
-lamderaApplicationElmJson =
-    { path = "elm.json"
-    , raw = """{
-    "type": "application",
-    "source-directories": [
-        "src"
-    ],
-    "elm-version": "0.19.1",
-    "dependencies": {
-        "direct": {
-            "elm/core": "1.0.0",
-            "lamdera/core": "1.0.0"
-        },
-        "indirect": {}
-    },
-    "test-dependencies": {
-        "direct": {},
-        "indirect": {}
-    }
-}"""
-    , project =
-        Elm.Project.Application
-            { elm = Elm.Version.one
-            , dirs = []
-            , depsDirect =
-                [ ( unsafePackageName "elm/core", Elm.Version.one )
-                , ( unsafePackageName "lamdera/core", Elm.Version.one )
-                ]
-            , depsIndirect = []
-            , testDepsDirect = []
-            , testDepsIndirect = []
-            }
-    }
-
-
-unsafePackageName : String -> Elm.Package.Name
-unsafePackageName packageName =
-    case Elm.Package.fromString packageName of
-        Just name ->
-            name
-
-        Nothing ->
-            -- unsafe, but if the generation went well, it should all be good.
-            unsafePackageName packageName
-                -- Disables the tail-call optimization, so that the test crashes if we enter this case
-                |> identity
-
-
-package : Project
-package =
-    Project.new
-        |> Project.addElmJson (createPackageElmJson ())
-
-
-createPackageElmJson : () -> { path : String, raw : String, project : Elm.Project.Project }
-createPackageElmJson _ =
-    case Decode.decodeString Elm.Project.decoder rawPackageElmJson of
-        Ok elmJson ->
-            { path = "elm.json"
-            , raw = rawPackageElmJson
-            , project = elmJson
-            }
-
-        Err err ->
-            Debug.todo ("Invalid elm.json supplied to test: " ++ Debug.toString err)
-
-
-rawPackageElmJson : String
-rawPackageElmJson =
-    """{
-    "type": "package",
-    "name": "author/package",
-    "summary": "Summary",
-    "license": "BSD-3-Clause",
-    "version": "1.0.0",
-    "exposed-modules": [
-        "Exposed"
-    ],
-    "elm-version": "0.19.0 <= v < 0.20.0",
-    "dependencies": {},
-    "test-dependencies": {}
-}"""
-
-
 details : List String
 details =
     [ "This module is never used. You may want to remove it to keep your project clean, and maybe detect some unused code in your project."
@@ -346,3 +217,132 @@ app = text ""
                 |> Review.Test.runWithProjectData lamderaApplication rule
                 |> Review.Test.expectNoErrors
     ]
+
+
+application : Project
+application =
+    Project.new
+        |> Project.addElmJson applicationElmJson
+
+
+lamderaApplication : Project
+lamderaApplication =
+    Project.new
+        |> Project.addElmJson lamderaApplicationElmJson
+
+
+applicationElmJson : { path : String, raw : String, project : Elm.Project.Project }
+applicationElmJson =
+    { path = "elm.json"
+    , raw = """{
+    "type": "application",
+    "source-directories": [
+        "src"
+    ],
+    "elm-version": "0.19.1",
+    "dependencies": {
+        "direct": {
+            "elm/core": "1.0.0"
+        },
+        "indirect": {}
+    },
+    "test-dependencies": {
+        "direct": {},
+        "indirect": {}
+    }
+}"""
+    , project =
+        Elm.Project.Application
+            { elm = Elm.Version.one
+            , dirs = []
+            , depsDirect = [ ( unsafePackageName "elm/core", Elm.Version.one ) ]
+            , depsIndirect = []
+            , testDepsDirect = []
+            , testDepsIndirect = []
+            }
+    }
+
+
+lamderaApplicationElmJson : { path : String, raw : String, project : Elm.Project.Project }
+lamderaApplicationElmJson =
+    { path = "elm.json"
+    , raw = """{
+    "type": "application",
+    "source-directories": [
+        "src"
+    ],
+    "elm-version": "0.19.1",
+    "dependencies": {
+        "direct": {
+            "elm/core": "1.0.0",
+            "lamdera/core": "1.0.0"
+        },
+        "indirect": {}
+    },
+    "test-dependencies": {
+        "direct": {},
+        "indirect": {}
+    }
+}"""
+    , project =
+        Elm.Project.Application
+            { elm = Elm.Version.one
+            , dirs = []
+            , depsDirect =
+                [ ( unsafePackageName "elm/core", Elm.Version.one )
+                , ( unsafePackageName "lamdera/core", Elm.Version.one )
+                ]
+            , depsIndirect = []
+            , testDepsDirect = []
+            , testDepsIndirect = []
+            }
+    }
+
+
+unsafePackageName : String -> Elm.Package.Name
+unsafePackageName packageName =
+    case Elm.Package.fromString packageName of
+        Just name ->
+            name
+
+        Nothing ->
+            -- unsafe, but if the generation went well, it should all be good.
+            unsafePackageName packageName
+                -- Disables the tail-call optimization, so that the test crashes if we enter this case
+                |> identity
+
+
+package : Project
+package =
+    Project.new
+        |> Project.addElmJson (createPackageElmJson ())
+
+
+createPackageElmJson : () -> { path : String, raw : String, project : Elm.Project.Project }
+createPackageElmJson _ =
+    case Decode.decodeString Elm.Project.decoder rawPackageElmJson of
+        Ok elmJson ->
+            { path = "elm.json"
+            , raw = rawPackageElmJson
+            , project = elmJson
+            }
+
+        Err err ->
+            Debug.todo ("Invalid elm.json supplied to test: " ++ Debug.toString err)
+
+
+rawPackageElmJson : String
+rawPackageElmJson =
+    """{
+    "type": "package",
+    "name": "author/package",
+    "summary": "Summary",
+    "license": "BSD-3-Clause",
+    "version": "1.0.0",
+    "exposed-modules": [
+        "Exposed"
+    ],
+    "elm-version": "0.19.0 <= v < 0.20.0",
+    "dependencies": {},
+    "test-dependencies": {}
+}"""
