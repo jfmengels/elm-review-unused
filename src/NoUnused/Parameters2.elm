83e0f150c8078689b23b3f8386d7eb237d1d50d2
Jeroen Engels <jfm.engels@gmail.com>
2021-08-11 23:46:20 +0200

    Move hardcoded location

--- src/NoUnused/Parameters2.elm
+++ src/NoUnused/Parameters2.elm
@@ -111,7 +111,7 @@ initialContext : Context
 initialContext =
     { scopes = []
     , scopesToCreate = RangeDict.empty
-    , knownRecursiveCalls = RangeDict.singleton { start = { row = 6, column = 21 }, end = { row = 6, column = 27 } } ()
+    , knownRecursiveCalls = RangeDict.empty
     }
 
 
@@ -131,12 +131,12 @@ declarationVisitor node context =
                         |> List.concatMap (getParametersFromPatterns NamedFunction)
             in
             ( []
-            , { context
-                | scopes = []
-                , scopesToCreate =
+            , { scopes = []
+              , scopesToCreate =
                     RangeDict.singleton
                         (declaration |> Node.value |> .expression |> Node.range)
                         declared
+              , knownRecursiveCalls = RangeDict.singleton { start = { row = 6, column = 21 }, end = { row = 6, column = 27 } } ()
               }
             )
 
