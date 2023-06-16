e62d329bcd39bdffdec0330f33fe9c9616926161
Simon Lydell <simon.lydell@gmail.com>
2023-06-16 10:41:37 +0200

    elm-format

--- src/String/Extra.elm
+++ src/String/Extra.elm
@@ -7,7 +7,7 @@ module String.Extra exposing (isCapitalized)
 {-| Check if the first character of a string is upper case, unicode aware.
 
 Note that `Char.isUpper` returns the correct result only for ASCII characters, even though it can be called with any Unicode character.
-See https://github.com/elm/core/pull/1138
+See <https://github.com/elm/core/pull/1138>
 
 -}
 isCapitalized : String -> Bool
@@ -15,10 +15,9 @@ isCapitalized string =
     case String.uncons string of
         Just ( char, _ ) ->
             -- 1. The character is its own upper variant.
-           (char == Char.toUpper char)
+            (char == Char.toUpper char)
                 -- 2. The character is not its own lower variant.
                 && (char /= Char.toLower char)
-
 
         Nothing ->
             False
