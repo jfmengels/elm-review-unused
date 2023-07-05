bfed1abf5fde4767b6f1a5e263cc1092e17e3835
Jeroen Engels <jfm.engels@gmail.com>
2023-07-05 13:02:03 +0200

    Don't report exports of ignored modules if they're also used in ignored modules

--- src/NoUnused/Exports.elm
+++ src/NoUnused/Exports.elm
@@ -396,43 +396,47 @@ errorsForModule helperTags projectContext { used, usedInIgnoredModules } moduleN
                 subAcc
 
             else if Set.member ( moduleName, name ) usedInIgnoredModules then
-                let
-                    what : String
-                    what =
-                        case element.elementType of
-                            Function ->
-                                "Exposed function or value"
+                if isModuleIgnored then
+                    subAcc
 
-                            TypeOrTypeAlias ->
-                                "Exposed type or type alias"
+                else
+                    let
+                        what : String
+                        what =
+                            case element.elementType of
+                                Function ->
+                                    "Exposed function or value"
 
-                            ExposedType _ ->
-                                "Exposed type"
-                in
-                Rule.errorForModule moduleKey
-                    { message = what ++ " `" ++ name ++ "` is never used in production code."
-                    , details =
-                        "This exposed element is only used in files/folders you ignore (e.g. the test folder), and should therefore be removed along with the places it's used in. This will help reduce the amount of code you will need to maintain."
-                            :: (case helperTags of
-                                    [] ->
-                                        [ "It is possible that this element is meant to enable work in your ignored folder (test helpers for instance), in which case you should keep it. To avoid this problem being reported again, you can annotate this element by including documentation annotations."
-                                        , "You have not configured this rule with any possible annotations though. Check out the documentation for this rule on how to enable that."
-                                        ]
+                                TypeOrTypeAlias ->
+                                    "Exposed type or type alias"
 
-                                    first :: rest ->
-                                        [ "It is possible that this element is meant to enable work in your ignored folder (test helpers for instance), in which case you should keep it. To avoid this problem being reported again, you can annotate this element by including documentation annotations:"
-                                        , """    {-| Some element.
+                                ExposedType _ ->
+                                    "Exposed type"
+                    in
+                    Rule.errorForModule moduleKey
+                        { message = what ++ " `" ++ name ++ "` is never used in production code."
+                        , details =
+                            "This exposed element is only used in files/folders you ignore (e.g. the test folder), and should therefore be removed along with the places it's used in. This will help reduce the amount of code you will need to maintain."
+                                :: (case helperTags of
+                                        [] ->
+                                            [ "It is possible that this element is meant to enable work in your ignored folder (test helpers for instance), in which case you should keep it. To avoid this problem being reported again, you can annotate this element by including documentation annotations."
+                                            , "You have not configured this rule with any possible annotations though. Check out the documentation for this rule on how to enable that."
+                                            ]
+
+                                        first :: rest ->
+                                            [ "It is possible that this element is meant to enable work in your ignored folder (test helpers for instance), in which case you should keep it. To avoid this problem being reported again, you can annotate this element by including documentation annotations:"
+                                            , """    {-| Some element.
     """
-                                            ++ formatTags first rest
-                                            ++ """
+                                                ++ formatTags first rest
+                                                ++ """
     -}
     yourElement = ...
 """
-                                        ]
-                               )
-                    }
-                    element.range
-                    :: subAcc
+                                            ]
+                                   )
+                        }
+                        element.range
+                        :: subAcc
 
             else
                 let
