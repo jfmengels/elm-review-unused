65bc1336c92cbda3b95539be07c09ac192e4beb6
Jeroen Engels <jfm.engels@gmail.com>
2023-07-16 10:09:50 +0200

    Say 'tests folder'

--- tests/NoUnused/ExportsTest.elm
+++ tests/NoUnused/ExportsTest.elm
@@ -1254,7 +1254,7 @@ a = A.unusedInProductionCode
                           , [ Review.Test.error
                                 { message = "Exposed function or value `unusedInProductionCode` is never used in production code."
                                 , details =
-                                    [ "This exposed element is only used in files/folders you ignore (e.g. the test folder), and should therefore be removed along with the places it's used in. This will help reduce the amount of code you will need to maintain."
+                                    [ "This exposed element is only used in files/folders you ignore (e.g. the tests folder), and should therefore be removed along with the places it's used in. This will help reduce the amount of code you will need to maintain."
                                     , "It is possible that this element is meant to enable work in your ignored folder (test helpers for instance), in which case you should keep it. To avoid this problem being reported again, please read the documentation on how to configure the rule."
                                     ]
                                 , under = "unusedInProductionCode"
@@ -1297,7 +1297,7 @@ a = A.unusedInProductionCode
                           , [ Review.Test.error
                                 { message = "Exposed function or value `unusedInProductionCode` is never used in production code."
                                 , details =
-                                    [ "This exposed element is only used in files/folders you ignore (e.g. the test folder), and should therefore be removed along with the places it's used in. This will help reduce the amount of code you will need to maintain."
+                                    [ "This exposed element is only used in files/folders you ignore (e.g. the tests folder), and should therefore be removed along with the places it's used in. This will help reduce the amount of code you will need to maintain."
                                     , "It is possible that this element is meant to enable work in your ignored folder (test helpers for instance), in which case you should keep it. To avoid this problem being reported again, you can:"
                                     , """- Include @helper in the documentation of the element
 - Include @test-helper in the documentation of the element
