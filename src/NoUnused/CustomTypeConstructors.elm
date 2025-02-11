a69479216987442cbadd1e932c3f7307c8232345
Jeroen Engels <jfm.engels@gmail.com>
2025-02-11 21:15:08 +0100

    PAss moduleKeys

--- src/NoUnused/CustomTypeConstructors.elm
+++ src/NoUnused/CustomTypeConstructors.elm
@@ -1110,6 +1110,7 @@ errorsForConstructors projectContext usedConstructors moduleName moduleKey const
                     , wasUsedInComparisons = Set.member ( moduleName, constructorInformation.name ) projectContext.wasUsedInComparisons
                     , isUsedInOtherModules = Set.member ( moduleName, constructorInformation.name ) projectContext.wasUsedInOtherModules
                     , fixesForRemovingConstructor = Dict.get ( moduleName, constructorInformation.name ) projectContext.fixesForRemovingConstructor |> Maybe.withDefault []
+                    , moduleKeys = projectContext.moduleKeys
                     }
                     constructorInformation
                     :: subAcc
@@ -1147,6 +1148,7 @@ errorForModule :
         , wasUsedInComparisons : Bool
         , isUsedInOtherModules : Bool
         , fixesForRemovingConstructor : List Fix
+        , moduleKeys : Dict ModuleNameAsString Rule.ModuleKey
         }
     -> ConstructorInformation
     -> Error scope
